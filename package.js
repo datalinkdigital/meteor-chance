07e45a1cb8c21580cb876516ffa0753569e7ead5
Jeroen van Veen <j.veenvan@gmail.com>
2013-11-28 16:47:07 +0100

    initial commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "Chance is a minimalist generator of random [1] strings, numbers, etc. to help reduce some monotony particularly while writing automated tests or anywhere else you need anything random."
+});
+
+Npm.depends({
+    "chance": "0.5.3"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files('chance.js', 'server');
+  if (api.export) {
+    api.export("Chance", "server");
+  }
+});
